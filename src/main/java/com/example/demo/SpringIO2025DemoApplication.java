e4f58c16d72379a2286591804d825b4ee9f7ff64
Konstantin Aleev <konstantin.aleev@jetbrains.com>
2025-05-20 16:45:58 +0200

    initial commit

---
+++ src/main/java/com/example/demo/SpringIO2025DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIO2025DemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringIO2025DemoApplication.class, args);
+  }
+
+}
